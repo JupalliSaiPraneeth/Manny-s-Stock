e961a9ec4c695e50085e2488cb241c92c6335568
JupalliSaiPraneeth <jupallisaipraneeth540@gmail.com>
2025-09-10 12:46:45 +0530

    Fix StoryGrid.jsx image imports and structure

--- src/Components/StoryGrid.jsx
+++ src/Components/StoryGrid.jsx
@@ -1,36 +1,43 @@
 import React from "react";
-// eslint-disable-next-line no-unused-vars
-import Buttabomma from "./Buttabomma.jpg";
-import Ghost from "./Dance_ghost.jpg";
-import kill from "./killme.jpg";
-import Blue from "./Blueboy.jpg";
-// eslint-disable-next-line no-unused-vars
-import sunshine from "./sunshine.jpg";
+import Buttabomma from "../assets/Buttabomma.jpg";
+import Ghost from "../assets/Dance_ghost.jpg";
+import Killme from "../assets/killme.jpg";
+import Blue from "../assets/Blueboy.jpg";
+import Sunshine from "../assets/sunshine.jpg";
 
-const StoryGrid = ()=>{
-  return(
+const StoryGrid = () => {
+  return (
     <div className="wrapper">
       <div className="box box1">
-          <h2 className="story-title">Buttabomma</h2>
-          <a href="https://www.wattpad.com/1555031226-rain-diaries-of-lost-love-buttabomma"><img className="cover-pic" src={Buttabomma} alt="butta" /></a>
+        <h2 className="story-title">Buttabomma</h2>
+        <a href="https://www.wattpad.com/1555031226-rain-diaries-of-lost-love-buttabomma" target="_blank" rel="noopener noreferrer">
+          <img className="cover-pic" src={Buttabomma} alt="Buttabomma" />
+        </a>
       </div>
+
       <div className="box box2">
-           <h2 className="story-title">DanceWithGhost</h2>
-          <a href="https://www.wattpad.com/1558293761-rain-diaries-of-lost-love-dancing-with-a-ghost"><img className="cover-pic" src={Ghost} alt="ghost" /></a>
+        <h2 className="story-title">Dance With Ghost</h2>
+        <a href="https://www.wattpad.com/1558293761-rain-diaries-of-lost-love-dancing-with-a-ghost" target="_blank" rel="noopener noreferrer">
+          <img className="cover-pic" src={Ghost} alt="Ghost" />
+        </a>
       </div>
+
       <div className="box box3">
-            <h2 className="story-title">Blue Boy</h2>
-          <img className="cover-pic" src={Blue} alt="boy" />
+        <h2 className="story-title">Blue Boy</h2>
+        <img className="cover-pic" src={Blue} alt="Blue Boy" />
       </div>
+
       <div className="box box4">
-             <h2 className="story-title">KillMe Softly</h2>
-          <img className="cover-pic" src={kill} alt="killme" />
+        <h2 className="story-title">Kill Me Softly</h2>
+        <img className="cover-pic" src={Killme} alt="Kill Me Softly" />
       </div>
-      <div className="box box4">
-             <h2 className="story-title">Me and the sunshine!</h2>
-            <a href="https://www.wattpad.com/story/401313025?utm_source=android&utm_medium=link&utm_content=share_reading&wp_page=reading&wp_uname=MrManny5"><img className="cover-pic" src={sunshine} alt="killme" /></a>
+
+      <div className="box box5">
+        <h2 className="story-title">Me and the Sunshine!</h2>
+        <a href="https://www.wattpad.com/story/401313025?utm_source=android&utm_medium=link&utm_content=share_reading&wp_page=reading&wp_uname=MrManny5" target="_blank" rel="noopener noreferrer">
+          <img className="cover-pic" src={Sunshine} alt="Sunshine" />
+        </a>
       </div>
-      
     </div>
   );
 };
